2669b79f35774c5a3fd2105853e5564f7724c0a0
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-09-19 09:17:36 -0700

    adding fixed tests

--- lender_books/tests.py
+++ lender_books/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase, RequestFactory
 from .models import Book
 
 class TestBookModel(TestCase):
-    def setup(self):
+    def setUp(self):
         self.book = Book.objects.create(title='A Sprinkle in Thyme', author='RL Burnside', year='1776', isbn='1234')
         Book.objects.create(title='My Cousin Lewis', author='John Ham', year='1969')
         Book.objects.create(title='The Lying, The Switch, and the War Robe', author='Beavis', year='2021')
@@ -15,7 +15,7 @@ class TestBookModel(TestCase):
         self.assertEqual(book.author, 'RL Burnside')
 
 class TestBookViews(TestCase):
-    def setup(self):
+    def setUp(self):
         self.request = RequestFactory()
         self.book_one = Book.objects.create(title='The First Book', author='Yes', year='2012')
         self.book_two = Book.objects.create(title='The Second Book', author='No', year='2016')
